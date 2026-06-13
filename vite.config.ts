9215ad52162861766e9e120a598f9fa786b4131e
madimade <madimade444@gmail.com>
2026-06-13 23:48:32 +0300

    fix: update base path to game-api

--- vite.config.ts
+++ vite.config.ts
@@ -17,20 +17,18 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  // 👇 يجب أن يكون الاسم هنا game-api ليطابق رابط جيت هاب الجديد
+  base: '/game-api/', 
+
   plugins: [
     figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
